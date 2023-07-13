63b836e3e1ae1a320a9785e2e6917391105ea536
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-07-13 13:28:26 +0300

    importing all employee controller

---
+++ src/controller/employee/index.employee.controller.ts
@@ -0,0 +1,12 @@
+import { createEmployee } from "./create.employee.controller";
+import { getEmployees, getEmployee } from "./get.employee.controller";
+import { updateEmployee } from "./update.employee.controller";
+import { deleteEmployee } from "./delete.employee.controller";
+
+export {
+  createEmployee,
+  getEmployees,
+  getEmployee,
+  updateEmployee,
+  deleteEmployee,
+};
