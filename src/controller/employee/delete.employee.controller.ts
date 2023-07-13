08cb06f04d6eceb22986e060a88934581aebe869
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-07-13 13:28:11 +0300

    Delete employee controller

---
+++ src/controller/employee/delete.employee.controller.ts
@@ -0,0 +1,15 @@
+import asyncHandler from "express-async-handler";
+import EmployeeModel from "../../model/employee";
+// import  user from "../../model/user"
+export const deleteEmployee = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const removed = await EmployeeModel.findByIdAndDelete(id);
+    if (!removed) throw Error("Something went wrong ");
+    res
+      .status(200)
+      .json({ message: "employee deleted successfully", success: true });
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
+  }
+});
