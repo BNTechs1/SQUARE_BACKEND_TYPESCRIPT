8305ffe0da1f07ae51a7c48c6f6f01822929c1c5
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-07-13 18:23:16 +0300

    db configuration updated

--- src/config/db_config.ts
+++ src/config/db_config.ts
@@ -1,30 +1,22 @@
-import { printConsole } from "../utils/development";
 import mongoose from "mongoose";
-const MONGO_DB_URI = process.env.MONGO_REMOTE_URL ||  process.env.MONGO_LOCAL_URL;
+// const MONGO_DB_URI =
+//   process.env.MONGO_REMOTE_URL || process.env.MONGO_LOCAL_URL;
 
 export const connectToDB = async () => {
   try {
-    printConsole(
-      { data: "Connecting to MongoDB ......" },
-      { textColor: "yellow" }
-    );
-    const DBConnection = await mongoose.connect("mongodb+srv://square:Bnsquare101@square.n0h2qw9.mongodb.net/square", {
-      // useNewUrlParser: true,
-      // useCreateIndex: true,
-      // useUnifiedTopology: true,
-      // useFindAndModify: false,
-    });
-
-    printConsole(
-      { data: `Database Connected : ${DBConnection.connection.host}` },
+    console.log("Connecting to MongoDB ......");
+    const DBConnection = await mongoose.connect(
+      "mongodb+srv://square:Bnsquare101@square.n0h2qw9.mongodb.net/square",
       {
-        textColor: "green",
+        // useNewUrlParser: true,
+        // useCreateIndex: true,
+        // useUnifiedTopology: true,
+        // useFindAndModify: false,
       }
     );
-  } catch (error) {
-    printConsole(error);
 
+    console.log(`Database Connected : ${DBConnection.connection.host}`);
+  } catch (error) {
     process.exit(1);
   }
 };
-
