d315705919362a42a208baafbe21820da6d8b8e2
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-07-14 16:22:59 +0300

    create inventory

---
+++ src/controller/inventory/create.inventoroy.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import InventoryModel from "../../model/inventory";
+
+export const create = async (req: Request, res: Response) => {
+  const { name, unit, quantity, rate, requiredValue } = req.body;
+
+  const verifyItem = await InventoryModel.findOne({
+    name: req.body.name,
+  });
+
+  if (!verifyItem) {
+    await new InventoryModel({
+      name,
+      unit,
+      quantity,
+      rate,
+      value: rate * quantity,
+      requiredValue,
+    }).save();
+    res.status(201).json({
+      message: `item added successfully`,
+      success: true,
+    });
+  } else {
+    res.status(404).json({
+      message: "item exist",
+      success: false,
+    });
+  }
+};
