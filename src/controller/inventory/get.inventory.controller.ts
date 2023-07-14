d6702a48868182e21f8d97175d0d6410a03bae2c
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-07-14 16:23:21 +0300

    get inventory, get inventories, get purchases completed

---
+++ src/controller/inventory/get.inventory.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+// import InventoryModel from "../../model/inventory";
+import {
+  getAllItems,
+  showItem,
+} from "../../utils/db_functions/inventory.db";
+// import  user from "../../model/user"
+
+export const getinventories = async (req: Request, res: Response) => {
+  const inventory = getAllItems()
+  res.status(200).send(inventory);
+};
+
+export const getinventory = async (req: Request, res: Response) => {
+  // const { id } = req.userData
+  const id = "1212112211";
+  const inventory = showItem(id);
+  res.status(200).send(inventory);
+};
+
+
+export const getPurchases = async (req:Request, res:Response) => {
+  const { id } = req.params
+  const inventory = await showItem(id)
+  if(!inventory){
+    return res.status(404).json({
+      message:"item not found"
+    })
+  } 
+  const stack = inventory.stack
+  if(stack.length === 0){
+    return res.status(404).json({
+      message:"no purchase made"
+    })
+  }
+  res.status(200).send(stack);
+}
