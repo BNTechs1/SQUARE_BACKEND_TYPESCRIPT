818a0418fa457ef2fdb6114e57a3ca6415d3c2cd
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-07-14 16:29:22 +0300

    authentication functions imported

--- src/controller/auth/index.auth.controller.ts
+++ src/controller/auth/index.auth.controller.ts
@@ -1,6 +1,6 @@
 import { register } from "./register.auth.controller";
 import { getUsers, getuser } from "./get.auth.controller";
-// import {  } from "./update.employee.controller";
+import { login } from "./login.auth.controller";
 // import { deleteEmployee } from "./delete.employee.controller";
 
-export { register, getuser, getUsers };
+export { register, getuser, getUsers, login };
