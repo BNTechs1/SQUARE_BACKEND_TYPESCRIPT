54bfe348e0381f555e2f070dd5662ba24f443cb2
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-07-14 16:21:49 +0300

    authentication route completed

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,13 +3,14 @@ import {
   register,
   getUsers,
   getuser,
+  login
 } from "../controller/auth/index.auth.controller";
 const router = express.Router();
 
 router.post("/create", register);
 router.get("/get", getUsers);
 router.get("/show/:id", getuser);
-// router.put("/update", updateEmployee);
+router.post("/login", login);
 // router.delete("/delete", deleteEmployee);
 
 export default router;
