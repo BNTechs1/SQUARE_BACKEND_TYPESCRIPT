56200b9e2f85a44f6456a60cf6f2ca50383d49d5
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-07-14 16:24:11 +0300

    imported inventory controllers

---
+++ src/controller/inventory/index.inventory.controller.ts
@@ -0,0 +1,13 @@
+import { create } from "./create.inventoroy.controller";
+import { getinventories, getinventory, getPurchases } from "./get.inventory.controller";
+import { update } from "./update.inventory.controller";
+import { deleteInventory } from "./delete.inventory.controller";
+
+export {
+    create,
+    getinventories,
+    getinventory,
+    getPurchases,
+    update,
+    deleteInventory,
+};
