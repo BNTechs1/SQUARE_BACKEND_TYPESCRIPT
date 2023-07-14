46096db859667eee8568fc8f0b17d91dc4d59b70
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-07-14 16:31:20 +0300

    removed files

--- src/model/category.ts
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const categorySchema = new Schema({
-  categoryId: { type: String, required: true },
-  categoryName: { type: String, required: true },
-});
-
-export default model("Category", categorySchema);
