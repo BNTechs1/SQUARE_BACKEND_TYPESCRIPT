68a0b0bfb6bc6216bdb1d2332c6b7114ef6d6f03
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-07-19 14:54:44 +0300

    imported menu functionalities

---
+++ src/controller/menu/index.menu.controller.ts
@@ -0,0 +1,19 @@
+import { createMenuType } from "./create/createmenuType.menu.controler";
+import { createMenuCat } from "./create/createmenuCat.menu.controller";
+import { createMenu,} from "./create/createMenu.menu.controller";
+import { createSize,} from "./create/createSize.menu.controller";
+import { createRecipe } from "./create/addRecipe.menu.controller";
+
+// import { createMenuType } from "./create/createSize.menu.controller";
+import { getMenusTypes, showMenuTypes } from "./get.menu.controller";
+
+
+export {
+        createMenuType,
+        createMenuCat,
+        createMenu,
+        getMenusTypes, 
+        showMenuTypes, 
+        createSize, 
+        createRecipe
+};
