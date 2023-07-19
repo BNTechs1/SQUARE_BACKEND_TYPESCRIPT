881cd634d25a4eea979b9bcdaef2addc7c4d1e8e
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-07-19 14:57:44 +0300

    imported all api's of the app

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 
 import MessageResponse from "../interfaces/MessageResponse";
 import emojis from "./emojis";
-import employee from "./employee";
-import auth from "./auth";
+import employee from "./employee.api";
+import auth from "./auth.api";
+import menu from "./menu.api";
 
-import inventory from "./inventory";
+
+import inventory from "./inventory.api";
 
 const router = express.Router();
 
@@ -19,5 +21,7 @@ router.use("/emojis", emojis);
 router.use("/employee", employee);
 router.use("/auth", auth);
 router.use("/inventory", inventory);
+router.use("/menu", menu);
+
 
 export default router;
