8bb3c3033544bb62f7d5688bb824024b1e7ce9d4
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2024-02-29 09:14:25 +0300

    cors option

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
-import cors from "cors";
-
+import { corsOptions } from "./config/corsOption";
+import cors from "cors"
 import * as middlewares from "./middlewares";
 import api from "./api";
 import MessageResponse from "./interfaces/MessageResponse";
@@ -11,10 +11,10 @@ import MessageResponse from "./interfaces/MessageResponse";
 config();
 
 const app = express();
-
 app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.get<MessageResponse>("/", (req, res) => {
---
+++ src/config/corsOption.ts
@@ -0,0 +1,15 @@
+import cors from 'cors'
+const allowedOrigins: string[] = ["https://sys.squarepizzaet.com"];
+
+export const corsOptions: cors.CorsOptions = {
+    origin: (origin: string | undefined, callback: (error: Error | null, allow?: boolean) => void) => {
+        if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+};
