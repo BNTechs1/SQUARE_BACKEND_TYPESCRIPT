255e90dbfbfe70c76166a2396a21f383beffc6ca
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2024-03-05 16:14:21 +0300

    cloudinary config

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,14 +1,17 @@
 import { v2 as cloudinary } from "cloudinary";
 import { config } from "dotenv";
 config();
+const CLOUD_NAME = "dphh50qtv"
+cosnt CLOUDINARY_API_KEY = "891473819878183"
+const CLOUDINARY_API_SECRET = "lsTqPXkv87kiCbpciYfdXmMBw4E"
 
 cloudinary.config({
   // eslint-disable-next-line no-undef
-  cloud_name: process.env.CLOUD_NAME,
+  cloud_name: CLOUD_NAME,
   // eslint-disable-next-line no-undef
-  api_key: process.env.CLOUDINARY_API_KEY,
+  api_key: CLOUDINARY_API_KEY,
   // eslint-disable-next-line no-undef
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret: CLOUDINARY_API_SECRET,
 });
 
 export const uploads = (file, folder) => {
